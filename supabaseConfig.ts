bfc4c9e810fc7358e8fb1ee6a8e3268dd9bb2d71
hawanatours-web <hawana.tours@gmail.com>
2025-12-10 23:16:57 +0200

    Update supabaseConfig.ts

--- supabaseConfig.ts
+++ supabaseConfig.ts
@@ -4,8 +4,8 @@
 
 export const supabaseConfig = {
   // Example: https://abcdefghijklm.supabase.co
-  supabaseUrl: "https://alqlejlrkhvqudpifiio.supabase.co", 
+  supabaseUrl: "hhttps://ajdmymhjlwfnrdbtpazh.supabase.co", 
   
   // Example: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...
-  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFscWxlamxya2h2cXVkcGlmaWlvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM4MzkyMjUsImV4cCI6MjA3OTQxNTIyNX0.35Sfs3Jrpoyd2N7D3sQpWfcmelEltkl7eBGu6BJeZ-g" 
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFqZG15bWhqbHdmbnJkYnRwYXpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUzOTYwNjcsImV4cCI6MjA4MDk3MjA2N30.NzUHlHCf2dOOtikbNiChg1AF_MmJxmRXXQWX5-a8YoU" 
 };
